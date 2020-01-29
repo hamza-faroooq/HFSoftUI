bf74445466c645be9348db11051368f9c8486c29
Hamza Farooq <hamza_faroooq@yahoo.com>
2020-01-30 02:03:47 +0500

    Update Constants.swift

--- HFSoftUI/HFSoftUI/Supporting Files/Constants.swift
+++ HFSoftUI/HFSoftUI/Supporting Files/Constants.swift
@@ -10,9 +10,9 @@ import UIKit
 
 let HFSoftUIColor = #colorLiteral(red: 0.8745098039, green: 0.8941176471, blue: 0.9333333333, alpha: 1) // for light color scheme
 
-//let HFSoftUIColor = #colorLiteral(red: 0.3411764706, green: 0.7568627451, blue: 0.8156862745, alpha: 1) // for dark color scheme
+//let HFSoftUIColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1) // for dark color scheme
     
 let isDarkColorScheme: Bool = false // Make this true or false depending upon your color scheme. Play with this to achieve better results
 
-let isUpperWhiteRequired: Bool = true // Make this true or false depending upon your color scheme. Play with this to achieve better results. If you are using dark scheme, and still Neumorphic Effect doesn't appear, make it true, fingers crossed... :-D // NOTE TAHT Neumorphic Effect WILL NOT APPEAR ON EVERY COLOR SCHEME
+let isUpperWhiteRequired: Bool = false // Make this true or false depending upon your color scheme. Play with this to achieve better results. If you are using dark scheme, and still Neumorphic Effect doesn't appear, make it true, fingers crossed... :-D // NOTE TAHT Neumorphic Effect WILL NOT APPEAR ON EVERY COLOR SCHEME
 
